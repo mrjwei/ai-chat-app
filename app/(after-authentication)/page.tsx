c1b591fbf25d8fbee230651579dc3835579703b7
Jesse Wei <jesseweijapan@gmail.com>
2024-11-09 15:48:28 +0800

    fix build error

--- app/(after-authentication)/page.tsx
+++ app/(after-authentication)/page.tsx
@@ -14,6 +14,6 @@ export default async function Page() {
   const systemMessages = await fetchSystemMessages(session.user.id!)
 
   return (
-    <ChatWindow thread={null} userId={session.user.id!} systemMessages={systemMessages} />
+    <ChatWindow thread={null} userId={session.user.id!} />
   )
 }
